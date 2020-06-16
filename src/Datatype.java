432cad75b03a8dd73971647e7ead69c3132981e8
amde11 <amdeasrat@gmail.com>
2020-06-15 20:24:44 -0800

    Data types

---
+++ src/Datatype.java
@@ -0,0 +1,23 @@
+public class Datatype {
+    public static void main(String[] args){
+
+        boolean isSkyisBlue = true;
+        byte myByte = 30;
+        char myChar = 'A';
+        short myShortVariable = -30000;
+        int myIntiger = 90000;
+        float myFloat = 7.42f;
+        double mydouble = -90.283333;
+        long myLongvariable = 9999998;
+
+        System.out.println("My boolean Variable :" + isSkyisBlue);
+        System.out.println("My integer plus 1k is equal to " + (myIntiger + 1000));
+        System.out.println("My Char variable is equal to : " + myChar);
+        System.out.println("My Short variable is equal to :" + myShortVariable);
+        System.out.println("My Integer variable is equal to :" + myIntiger);
+        System.out.println("My Float variable is equal to :" + myFloat);
+        System.out.println("My Double variable is equal to :" + mydouble);
+        System.out.println("My Long variable is equal to :" + myLongvariable);
+
+    }
+}
